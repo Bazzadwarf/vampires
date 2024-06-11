90a0a4c515bd015990ba8ff8e6267e0ca5bdd219
baz <bazzadobs@live.co.uk>
2024-06-12 00:34:11 +0100

    Update Project settings

---
+++ Source/vampires/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VampireCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAMPIRES_API APlayerCharacter : public AVampireCharacter
+{
+	GENERATED_BODY()
+	
+};
