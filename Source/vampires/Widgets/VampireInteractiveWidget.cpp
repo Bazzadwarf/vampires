d6b143b7b1300b1a76fe67b677835bebdfd18f95
baz <bazzadobs@live.co.uk>
2025-05-07 00:57:23 +0100

    Add VampireInteractiveWidget

---
+++ Source/vampires/Widgets/VampireInteractiveWidget.cpp
@@ -0,0 +1,58 @@
+﻿// Louis Hobbs | 2024-2025
+
+
+#include "VampireInteractiveWidget.h"
+
+#include "Components/TextBlock.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Kismet/GameplayStatics.h"
+
+void UVampireInteractiveWidget::SetReturnScreen(UUserWidget* userWidget)
+{
+	if (userWidget)
+	{
+		PreviousScreen = userWidget;
+	}
+}
+
+void UVampireInteractiveWidget::PlayHoveredSound()
+{
+	if (ButtonHoveredSound)
+	{
+		UGameplayStatics::PlaySound2D(GetWorld(), ButtonHoveredSound);
+	}
+}
+
+void UVampireInteractiveWidget::SetTextBlockHovered(UTextBlock* TextBlock)
+{
+	TextBlock->SetColorAndOpacity(FSlateColor(ButtonHoveredTextColor));
+}
+
+void UVampireInteractiveWidget::SetTextBlockUnhovered(UTextBlock* TextBlock)
+{
+	TextBlock->SetColorAndOpacity(FSlateColor(ButtonUnhoveredTextColor));
+}
+
+void UVampireInteractiveWidget::PlayUnhoveredSound()
+{
+	if (ButtonUnhoveredSound)
+	{
+		UGameplayStatics::PlaySound2D(GetWorld(), ButtonUnhoveredSound);
+	}
+}
+
+void UVampireInteractiveWidget::PlayClickedSound()
+{
+	if (ButtonClickedSound)
+	{
+		UGameplayStatics::PlaySound2D(GetWorld(), ButtonClickedSound);
+	}
+}
+
+void UVampireInteractiveWidget::ReturnToPreviousScreen()
+{
+	GEngine->GameUserSettings->ApplySettings(false);
+
+	this->RemoveFromParent();
+	PreviousScreen->AddToViewport();
+}
---
+++ Source/vampires/Widgets/VampireInteractiveWidget.h
@@ -0,0 +1,62 @@
+﻿// Louis Hobbs | 2024-2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VampireInteractiveWidget.generated.h"
+
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class VAMPIRES_API UVampireInteractiveWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<USoundBase> ButtonHoveredSound;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FLinearColor ButtonHoveredTextColor = {0, 1, 0, 1};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FLinearColor ButtonUnhoveredTextColor = {1, 1, 1, 1};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<USoundBase> ButtonUnhoveredSound;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<USoundBase> ButtonClickedSound;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UUserWidget> PreviousScreen;
+
+public:
+
+	UFUNCTION()
+	void SetReturnScreen(UUserWidget* userWidget);
+
+protected:
+	UFUNCTION()
+	void PlayHoveredSound();
+
+	UFUNCTION()
+	void SetTextBlockHovered(UTextBlock* TextBlock);
+
+	UFUNCTION()
+	void SetTextBlockUnhovered(UTextBlock* TextBlock);
+
+	UFUNCTION()
+	void PlayUnhoveredSound();
+
+	UFUNCTION()
+	void PlayClickedSound();
+
+	UFUNCTION()
+	void ReturnToPreviousScreen();
+};
