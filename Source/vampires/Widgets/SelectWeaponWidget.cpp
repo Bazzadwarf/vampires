7d8c59d3fe67305e0fe12ab34bc5d2e575b5fdf3
baz <bazzadobs@live.co.uk>
2025-06-12 23:16:39 +0100

    Misc changes

---
+++ Source/vampires/Widgets/SelectWeaponWidget.cpp
@@ -0,0 +1,20 @@
+﻿// Louis Hobbs | 2024-2025
+
+
+#include "SelectWeaponWidget.h"
+
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SelectWeaponWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/vampires/Widgets/SelectWeaponWidget.h
@@ -0,0 +1,22 @@
+﻿// Louis Hobbs | 2024-2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class VAMPIRES_API SelectWeaponWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SelectWeaponWidget)
+		{
+		}
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+};
