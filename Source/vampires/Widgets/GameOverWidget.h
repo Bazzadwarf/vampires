44e6876a8c16dddca10a1facb3bb63d4412cffdd
baz <bazzadobs@live.co.uk>
2025-08-15 22:26:54 +0100

    Simplify class definition

--- Source/vampires/Widgets/GameOverWidget.h
+++ Source/vampires/Widgets/GameOverWidget.h
@@ -17,30 +17,29 @@ class VAMPIRES_API UGameOverWidget : public UVampireInteractiveWidget
 {
 	GENERATED_BODY()
 
-protected:
-	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UButton> ReturnButton;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	TObjectPtr <UTextBlock> ReturnBlock;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> ReturnBlock;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	TObjectPtr <UTextBlock> LevelBlock;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> LevelBlock;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	TObjectPtr <UTextBlock> TimerBlock;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TimerBlock;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	TObjectPtr <UTextBlock> KillBlock;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> KillBlock;
 
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	TObjectPtr <UTextBlock> GoldBlock;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> GoldBlock;
 
 	virtual void NativeConstruct() override;
 
 public:
 	void SetGameInfo(int Level, float Timer, int Kill, int Gold);
-	
+
 private:
 	UFUNCTION()
 	void ReturnButtonOnClicked();
